3468aa9ba896041bc3cd1daa3da1e59d8f9b2855
Juan Garcia <garcia.solutions@gmail.com>
2024-05-22 17:12:27 -0300

    branch develoment

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Hello World! este es el branch que vamos a usar para probar antes de pasar main" << endl;
+
     return 0;
 }
